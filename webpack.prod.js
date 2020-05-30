7ef416b3d534f9102809b907fd54638ffc691523
André Justo <andrefjusto@gmail.com>
2020-05-30 23:37:24 +0100

    chore: setup webpack-bundle-analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,14 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 const merge = require("webpack-merge");
 const { ui, main } = require("./webpack.common.js");
 
 const uiConfig = merge(ui, {
   mode: "production",
   plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "disabled",
+      generateStatsFile: true,
     }),
   ],
 });
